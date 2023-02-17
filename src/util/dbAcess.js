8610db6014bffbc95bcc3dbe736e226a1835c520
igordbauer <igordbauer@gmail.com>
2023-02-17 10:25:29 -0300

    setup-enviroument-vars

--- src/util/dbAcess.js
+++ src/util/dbAcess.js
@@ -1,10 +1,8 @@
 import { MongoClient } from "mongodb";
 
 export const connectToDatabase = async () => {
-  const url =
-    "mongodb+srv://igordbauer:bnkNW4Bku4BJ7C6R@cluster0.mkk7l8p.mongodb.net/nextapi?retryWrites=true&w=majority";
   const client = await MongoClient.connect(
-    url
+    `mongodb+srv://${process.env.MONGODB_CREDENTIALS}@cluster0.mkk7l8p.mongodb.net/nextapi?retryWrites=true&w=majority`
     // "mongodb://0.0.0.0:27017/nextapi"
   );
   return client;
